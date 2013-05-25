4e8a1ccbf5e836242bdc23324c86a6c1a84f9515
Jonathan Pulice <jon.pulice@gmail.com>
2013-05-25 14:40:29 -0400

    User Auth Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
 
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -44,6 +45,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User Deleted Successfully"
+    redirect_to users_url
+  end
+  
 
 private
 
@@ -56,4 +63,9 @@ private
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
   end
+  
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+    
 end

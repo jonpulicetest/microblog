93f3460e159b03b5dde05608bf49c683706a4d7f
Jonathan Pulice <jon.pulice@gmail.com>
2013-05-26 02:19:08 -0400

    Being Relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ActiveRecord::Base
   has_secure_password
  
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: followed
  
   #Callbacks
   before_save { |user| user.email = email.downcase }
@@ -37,6 +39,19 @@ class User < ActiveRecord::Base
   def feed
     Micropost.where('user_id = ?', id)
   end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+  
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
+
   
   #Private Defs
 private
